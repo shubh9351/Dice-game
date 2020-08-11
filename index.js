082a941ee4c9531c21af7e9951d4cd14d6babc3d
shubham prabhakar jiotode <jiotodeshubham@gmail.com>
2020-08-11 12:15:13 +0530

    intial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+var randomImagesrc = "images/dice"+ randomNumber1 +".png";
+var image1= document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImagesrc);
+
+var randomNumber2 = Math.floor(Math.random() * 6) +1;
+var randomImagesrc2= "images/dice" + randomNumber2 +".png";
+var image2= document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImagesrc2);
+
+if (randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML="🎉Player1 Wins!";
+}else if (randomNumber1 < randomNumber2){
+  document.querySelector("h1").innerHTML="🎉Player2 Wins!";
+}else {
+  document.querySelector("h1").innerHTML="Draw!";
+}
